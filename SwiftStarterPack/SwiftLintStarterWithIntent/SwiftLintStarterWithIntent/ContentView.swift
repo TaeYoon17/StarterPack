b0e49ac51152a2ee7c52149d6be56e1e81382768
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2025-04-08 20:29:35 +0900

    [Feat] SwiftLint 적용

---
+++ SwiftStarterPack/SwiftLintStarterWithIntent/SwiftLintStarterWithIntent/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftLintStarterWithIntent
+//
+//  Created by Greem on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .padding(.bottom, 23)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftStarterPack/SwiftLintStarterWithIntent/SwiftLintStarterWithIntent/SwiftLintStarterWithIntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintStarterWithIntentApp.swift
+//  SwiftLintStarterWithIntent
+//
+//  Created by Greem on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintStarterWithIntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
